26c7392c94bba823e47ff5696c768f5026fca687
Samuel Lim <samuel@curium.sg>
2023-06-09 23:26:46 +0800

    Added header button

--- App.js
+++ App.js
@@ -10,10 +10,17 @@ import {
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./component/BlockRGB";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import ColourScreen from "./component/ColourScreen";
 
 function HomeScreen({ navigation }) {
   const [colorArray, setColorArray] = useState([]);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <Button onPress={addColor} title="+" />,
+    });
+  });
 
   function renderItem({ item }) {
     let { red, green, blue } = item;
@@ -38,26 +45,9 @@ function HomeScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <Button onPress={addColor} title="Add colour" />
+      {/* <Button onPress={addColor} title="Add colour" /> */}
+      <Button onPress={() => setColorArray([])} title="Reset colours" />
       <FlatList style={styles.list} data={colorArray} renderItem={renderItem} />
-    </View>
-  );
-}
-
-function ColourScreen({ route }) {
-  const { red, green, blue } = route.params;
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: `rgb(${red}, ${green}, ${blue})`,
-      }}
-    >
-      <Text>Red: {red}</Text>
-      <Text>Green: {green}</Text>
-      <Text>Blue: {blue}</Text>
     </View>
   );
 }
@@ -68,7 +58,13 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerTitleAlign: "center" }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerRight: () => <Button title="+" />,
+          }}
+        />
         <Stack.Screen name="Colour" component={ColourScreen} />
       </Stack.Navigator>
     </NavigationContainer>
