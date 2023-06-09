f3d17d417fb7382ab94af741065629a6f87570bb
slye20 <samuellim2011@gmail.com>
2023-06-09 23:26:46 +0800

    Added header button

---
+++ component/ColourScreen.js
@@ -0,0 +1,23 @@
+import { View, Text } from "react-native";
+
+export default function ColourScreen({ route }) {
+  const { red, green, blue } = route.params;
+  const darkMode = red + green + blue < 400 ? true : false;
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: `rgb(${red}, ${green}, ${blue})`,
+      }}
+    >
+      <Text style={{ color: darkMode ? "white" : "black" }}>Red: {red}</Text>
+      <Text style={{ color: darkMode ? "white" : "black" }}>
+        Green: {green}
+      </Text>
+      <Text style={{ color: darkMode ? "white" : "black" }}>Blue: {blue}</Text>
+    </View>
+  );
+}
